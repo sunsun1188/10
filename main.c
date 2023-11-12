bf3f51ec002f1de8eef1a0b8ebc22363c5c91be0
sunsun1188 <eunsun08110811@gmail.com>
2023-11-12 19:44:36 +0900

    실습3

--- main.c
+++ main.c
@@ -3,19 +3,11 @@
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+void main(void) {
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	int i=0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	strcpy(dst, src);
 	
-	printf("%s\n",str);
-	
-	while (str[i]!='\0') {
-		printf("%c",str[i]);
-		i++; 
-	}	
-} 
+	printf("copied string : %s", dst);
+}
