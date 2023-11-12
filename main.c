cc2cbdbf8045c74d1a238e0d077a7584be831737
sunsun1188 <eunsun08110811@gmail.com>
2023-11-12 21:33:37 +0900

    실습5

--- main.c
+++ main.c
@@ -3,8 +3,20 @@
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void) {
-	char str[30] = "happy C programming";
+int main(void) {
+	FILE* fp;
+	char word[100];
 	
-	printf("문자열\"%s\"의 길이 : %i", str, strlen(str));
-}
+	fp = fopen("sample.txt","w");
+	if (fp == NULL) {
+		printf("파일을 열 수 없습니다.\n");
+		return 1;
+	}
+	int i;
+	for (i=0; i<3; i++) {
+		printf("Input a word: ");
+		scanf("%s",word);
+		fprintf(fp,"%s\n",word);	
+	}
+	fclose(fp);
+} 
